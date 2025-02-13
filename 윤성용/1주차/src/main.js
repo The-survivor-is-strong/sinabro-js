5674562714ac192c34d7d1ff21f31b4e9ce424c5
tty5799 <iop017779@gmail.com>
2025-02-13 23:30:01 +0900

    주차별 폴더 정리

--- 윤성용/1주차/src/main.js
+++ 윤성용/1주차/src/main.js
@@ -2,11 +2,15 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
+import {test} from './module.js'
+import {test1} from './module1.js'
+import {test2} from './module2.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
     <a href="https://vite.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
+      ${test}
     </a>
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
